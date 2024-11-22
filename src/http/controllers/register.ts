1f6f116838282198bbc10a7fa43def72d04615c1
Ytalo Lopes <lopessoftec@gmail.com>
2024-11-22 14:11:08 -0300

    Caso de uso de registro

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -12,25 +12,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(request.body)
 
-    const password_hash = await hash(password, 6)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    })
-
-    if(userWithSameEmail){
-        return reply.status(409).send()
-    }
-
-    await prisma.user.create({
-        data: {
+    try {
+        await registerUseCase({
             name,
             email,
-            password_hash
-        }
-    })
+            password
+        })
+    } catch (error) {
+        return reply.status(409).send()
+    }
 
     return reply.status(201).send()
 }
---
+++ src/use-cases/register.ts
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
+
+interface RegisterUseCaseRequest {
+    name: string
+    email: string
+    password: string
+}
+
+export async function registerUseCase({name, email, password}: RegisterUseCaseRequest){
+
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if(userWithSameEmail){
+        throw new Error('User with same email already exists.')
+    }
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash
+        }
+    })
+}
