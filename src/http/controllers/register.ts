e82a5475ce3502d55a89edccf90286588577d1f6
Ytalo Lopes <lopessoftec@gmail.com>
2024-11-22 21:33:53 -0300

    Handler de erros global

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -27,7 +27,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       return reply.status(409).send({ message: error.message })
     }
 
-    return reply.status(500).send() // TODO fix me
+    // return reply.status(500).send() // TODO fix me
+    throw error
   }
 
   return reply.status(201).send()
